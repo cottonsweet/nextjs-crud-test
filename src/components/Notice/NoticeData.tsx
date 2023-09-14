838762000a88bff02edf283e48708ca9022c16bf
lk1 <whitecandy777@naver.com>
2023-09-15 01:28:31 +0900

    파일 변경 수정

--- src/components/Notice/NoticeData.tsx
+++ src/components/Notice/NoticeData.tsx
@@ -1,20 +1,19 @@
-// // Dummy Data
-// import { NoticeDataList } from "../../../data/data";
+// Dummy Data
+import { NoticeDataList } from "../../../data/data";
 
 // Components
 import NoticeCard from "./NoticeCard";
-// test
 
 export default function NoticeData() {
   return (
     <ul className="border-y-2 border-[#DEDEDE] h-auto py-3 px-4 mt-10 flex">
-      {/* {NoticeDataList.map((notice, _) => {
+      {NoticeDataList.map((notice, _) => {
         return (
           <li key={notice.id}>
             <NoticeCard notice={notice} />
           </li>
         );
-      })} */}
+      })}
     </ul>
   );
 }
