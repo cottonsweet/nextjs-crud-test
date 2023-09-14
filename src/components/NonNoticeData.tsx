50ffd40367a6a339f1a327dd934cc1643632c6af
lk1 <whitecandy777@naver.com>
2023-09-15 00:46:05 +0900

    공지사항 non data 컴포넌트, 공지사항 임시 데이터 컴포넌트 추가

---
+++ src/components/NonNoticeData.tsx
@@ -0,0 +1,7 @@
+export default function NonNoticeData() {
+  return (
+    <div className="border-y-2 border-[#DEDEDE] h-auto mt-10 py-20">
+      <p className="text-[#707070] text-center">공지사항이 없습니다.</p>
+    </div>
+  );
+}
---
+++ src/components/NoticeCard.tsx
@@ -0,0 +1,7 @@
+export default function NoticeCard() {
+  return (
+    <div className="border-y-2 border-[#DEDEDE] h-auto py-3 px-4 mt-10 flex">
+      공지사항 데이터 카드
+    </div>
+  );
+}
