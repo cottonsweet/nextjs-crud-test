928f1952b701caaa02c6fc62d7d42b6d2db9219f
[LK1] <[ssomcandy777@gmail.com]>
2023-09-18 17:55:13 +0900

    Feat: 임시용 post api 기능 추가

--- src/app/api/notice/route.ts
+++ src/app/api/notice/route.ts
@@ -7,3 +7,15 @@ export async function GET() {
   const notices = await prisma.notice.findMany();
   return new Response(JSON.stringify(notices));
 }
+
+export async function POST() {
+  const notice = await prisma.notice.create({
+    data: {
+      id: 2,
+      createdAt: new Date(),
+      title: "안녕하세요!",
+      body: "본문 테스트.",
+    },
+  });
+  return new Response(JSON.stringify("일단 포스트는.."));
+}
