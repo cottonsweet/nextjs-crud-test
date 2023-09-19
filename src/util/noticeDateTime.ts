c4c2f53a209ca8f2f64547a6e81274169c3e49b0
sstipdev <whitecandy777@naver.com>
2023-09-19 17:44:09 +0900

    Feat: 공지사항 날짜 시간 계산 함수 기능 추가

---
+++ src/util/noticeDateTime.ts
@@ -0,0 +1,31 @@
+const noticeDateTiem = (currentDate: string) => {
+  // 초
+  const sec = 1;
+  // 분
+  const min = sec * 60;
+  // 시
+  const hour = min * 60;
+  // 일
+  const day = hour * 24;
+
+  const today = new Date();
+  const elapsedTime = Math.trunc(
+    (today.getTime() - Number(currentDate)) / 1000
+  );
+  let elapsedText = "";
+  if (elapsedTime < sec) {
+    elapsedText = "방금 전";
+  } else if (elapsedTime < min) {
+    elapsedText = elapsedTime + "초 전";
+  } else if (elapsedTime < hour) {
+    elapsedText = Math.trunc(elapsedTime / min) + "분 전";
+  } else if (elapsedTime < day) {
+    elapsedText = Math.trunc(elapsedTime / hour) + "시간 전";
+  } else if (elapsedTime < day * 15) {
+    elapsedText = Math.trunc(elapsedTime / day) + "일 전";
+  }
+
+  return elapsedText;
+};
+
+export default noticeDateTiem;
