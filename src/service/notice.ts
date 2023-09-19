7f55bfbf71d523488e37d80eb2afbae9db7922af
sstipdev <whitecandy777@naver.com>
2023-09-19 17:24:38 +0900

    Feat: 공지사항 post 서비스 기능 추가

--- src/service/notice.ts
+++ src/service/notice.ts
@@ -1,9 +1,29 @@
+export interface NoticeReq {
+  createAt: Date;
+  title: string;
+  desc: string;
+}
+
 export async function getNoticeData() {
   const res = await fetch("/api/notice");
   const data = res.json();
 
   if (!res.ok) {
     throw new Error("서버 요청에 실패하였습니다 !");
+  }
+
+  return data;
+}
+
+export async function postNoticeData(notice: NoticeReq) {
+  const res = await fetch("/api/notice", {
+    method: "POST",
+    body: JSON.stringify(notice),
+  });
+  const data = res.json();
+
+  if (!res.ok) {
+    throw new Error("서버 요청에 실패하였습니다.");
   }
 
   return data;
